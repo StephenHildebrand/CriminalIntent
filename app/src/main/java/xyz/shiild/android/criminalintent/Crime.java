90be65bacf9dbe057937a52ad42d364f2965891e
Stephen Hildebrand <sfhildeb@gmail.com>
2016-06-29 15:48:07 -0400

    Adjusted header template. Added UUID, Title

--- app/src/main/java/xyz/shiild/android/criminalintent/Crime.java
+++ app/src/main/java/xyz/shiild/android/criminalintent/Crime.java
@@ -1,7 +1,35 @@
 package xyz.shiild.android.criminalintent;
 
+import java.util.UUID;
+
 /**
- * Created by Stephen on 6/29/2016.
+ *
+ * @author Stephen
+ * @version 6/29/2016
  */
 public class Crime {
+    /** Unique class identifier. */
+    private UUID mID;
+    /** Title of the Crime class. */
+    private String mTitle;
+
+    /**
+     * Crime class constructor.
+     */
+    public Crime() {
+        // Generate unique identifier
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getID() {
+        return mID;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
 }
