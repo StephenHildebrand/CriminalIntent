c8ff41cd825ddd3407412726523145db768bb1ff
Stephen <sfhildeb@gmail.com>
2016-07-10 21:28:49 -0400

    New controller class fragment for a CrimeList.

---
+++ app/src/main/java/xyz/shiild/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,11 @@
+package xyz.shiild.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * @author Stephen Hildebrand
+ * @version 7/10/2016
+ */
+public class CrimeListFragment extends Fragment {
+    // TBC (To Be Completed)
+}
