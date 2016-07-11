5ffd4cc8492ff465c9eab4db71c7c50e18ebc499
Stephen <sfhildeb@gmail.com>
2016-07-10 21:28:12 -0400

    Set CrimeActivity as sublcass of new SingleFragmentActivity abstract class.

--- app/src/main/java/xyz/shiild/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/xyz/shiild/android/criminalintent/CrimeActivity.java
@@ -1,66 +1,14 @@
 package xyz.shiild.android.criminalintent;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class CrimeActivity extends FragmentActivity {
-
+/**
+ * @author Stephen Hildebrand
+ * @version 7/10/2016
+ */
+public class CrimeActivity extends SingleFragmentActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
-
-        FragmentManager fm = getSupportFragmentManager();
-
-        // Give FragmentManager a fragment to manage, retrieved by container view ID
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-
-        if (fragment == null) {
-            fragment = new CrimeFragment();
-            // Create and commit a fragment transaction.
-            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
-        }
-
+    protected Fragment createFragment() {
+        return new CrimeFragment();
     }
-
-
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();f
-//            }
-//        });
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_crime, menu);
-//        return true;
-//    }
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
 }
