53785d5b93bf46e97a60a0e9f15ccddbb4bc43db
Stephen <sfhildeb@gmail.com>
2016-07-10 23:40:14 -0400

    Modified CrimeListFragment to use the newly created fragment_crime_list View.

--- app/src/main/java/xyz/shiild/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/xyz/shiild/android/criminalintent/CrimeListFragment.java
@@ -1,11 +1,27 @@
 package xyz.shiild.android.criminalintent;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
+ * Fragment that uses the fragment_crime_list View.
  * @author Stephen Hildebrand
  * @version 7/10/2016
  */
 public class CrimeListFragment extends Fragment {
-    // TBC (To Be Completed)
+    private RecyclerView mCrimeRecyclerView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+        mCrimeRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
+        mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return view;
+    }
 }
