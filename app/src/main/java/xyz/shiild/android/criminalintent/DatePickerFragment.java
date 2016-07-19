50c9900eb0714569fa110a85e55d76babe495419
Stephen <sfhildeb@gmail.com>
2016-07-19 15:27:08 -0400

    12.7 Extracted the date and initialized DatePicker.

--- app/src/main/java/xyz/shiild/android/criminalintent/DatePickerFragment.java
+++ app/src/main/java/xyz/shiild/android/criminalintent/DatePickerFragment.java
@@ -6,6 +6,10 @@ import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  *
@@ -13,6 +17,39 @@ import android.view.View;
  * @version 7/16/2016
  */
 public class DatePickerFragment extends DialogFragment {
+    /** Id for the date argument.  */
+    private static final String ARG_DATE = "date";
+    /** The DatePicker object. */
+    private DatePicker mDatePicker;
+
+    /**
+     * To get data into the DatePickerFragment, the date is stashed in its arguments bundle
+     * where it is able to be accessed. The arguments are created and set in the newInstance()
+     * method which replaces the fragment constructor.
+     *
+     # Adobe Blocker
+     127.0.0.1 lmlicenses.wip4.adobe.com
+     127.0.0.1 lm.licenses.adobe.com
+     127.0.0.1 na1r.services.adobe.com
+     127.0.0.1 hlrcv.stage.adobe.com
+     127.0.0.1 practivate.adobe.com
+     127.0.0.1 activate.adobe.com
+
+     # Malwarebytes Blocker
+     0.0.0.0 keystone.mwbsys.com
+     *
+     * @param date The date of the crime.
+     * @return The Fragment now containing the bundle in its arguments.
+     */
+    public static DatePickerFragment newInstance(Date date) {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_DATE, date);
+
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setArguments(args);
+
+        return fragment;
+    }
 
     /**
      * Creates the alert dialog. The AlertDialog.Builder class provides a fluent interface for
@@ -27,8 +64,22 @@ public class DatePickerFragment extends DialogFragment {
      */
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Initialize the DatePicker using the timestamp info held in the Date.
+        Date date = (Date)getArguments().getSerializable(ARG_DATE);
+        // Create calendar object using the Date to configure it.
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        // Retrieve the needed info from the calendar.
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
         // Inflate the view
         View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date, null);
+
+        // Initialize the DatePicker using the Date from the arguments and a Calendar.
+        mDatePicker = (DatePicker)v.findViewById(R.id.dialog_date_date_picker);
+        mDatePicker.init(year, month, day, null);
 
         return new AlertDialog.Builder(getActivity())
                 .setView(v) // Set the dialog's View.
