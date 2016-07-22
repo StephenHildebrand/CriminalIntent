271f0cf030af179234aefcb290d73b3c376ae04c
Stephen <sfhildeb@gmail.com>
2016-07-21 20:43:17 -0400

    13.9 Define onCreateOptionsMenu and OnCreate with setHasOptionsMenu(true).

--- app/src/main/java/xyz/shiild/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/xyz/shiild/android/criminalintent/CrimeListFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
@@ -25,6 +27,13 @@ public class CrimeListFragment extends Fragment {
     private CrimeAdapter mAdapter;
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Tell FragmentManager that CrimeListFragment needs to receive menu callbacks.
+        setHasOptionsMenu(true);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
         mCrimeRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
@@ -41,6 +50,18 @@ public class CrimeListFragment extends Fragment {
     public void onResume() {
         super.onResume();
         updateUI();
+    }
+
+    /**
+     * Creates the options menu by inflating the menu in fragment_crime_list.
+     *
+     * @param menu The menu instance to populate
+     * @param inflater The object to inflate the menu
+     */
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_crime_list, menu);
     }
 
     /**
