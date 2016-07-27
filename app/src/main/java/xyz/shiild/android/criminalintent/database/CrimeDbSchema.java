1da784f57373baaf5df7a9649a253df8984ea4aa
Stephen <sfhildeb@gmail.com>
2016-07-27 14:33:25 -0400

    Defined CrimeTable and the table columsn in Cols.

---
+++ app/src/main/java/xyz/shiild/android/criminalintent/database/CrimeDbSchema.java
@@ -0,0 +1,27 @@
+package xyz.shiild.android.criminalintent.database;
+
+/**
+ * @author Stephen Hildebrand
+ * @version 7/27/2016
+ */
+public class CrimeDbSchema {
+
+    /**
+     * The CrimeTable class exists only to define the String constants needed to describe
+     * the moving pieces of the table definition.
+     */
+    public static final class CrimeTable {
+        /** The name of the table in the database. */
+        public static final String NAME = "crimes";
+
+        /**
+         *
+         */
+        public static final class Cols {
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+        }
+    }
+}
